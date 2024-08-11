4174b7442b767e06c6353ecbc0066c3e28afd654
7x11x13 <x7x11x13@gmail.com>
2024-08-11 15:02:02 -0400

    Update links

--- build.py
+++ build.py
@@ -14,28 +14,104 @@ class LinkList:
     items: list[LinkItem]
 
 lists = [
-    LinkList("web-projects", "web apps", [
-        LinkItem("https://7x11x13.xyz/last-fm-overlap/", "last-fm-overlap", "view taste overlap of two last.fm users"),
-        LinkItem("https://7x11x13.xyz/sc-banner/", "sc-banner", "create seamless soundcloud banners")
-    ]),
-    LinkList("desktop-projects", "desktop apps", [
-        LinkItem("https://github.com/7x11x13/songs-to-youtube", "songs-to-youtube", "convert songs to videos and upload them to youtube"),
-        LinkItem("https://github.com/7x11x13/free-bandcamp-downloader", "free-bandcamp-downloader", "download $0 minimum music from bandcamp"),
-        LinkItem("https://github.com/7x11x13/bandcamp-auto-uploader", "bandcamp-auto-uploader", "upload albums to bandcamp")
-    ]),
-    LinkList("browser-projects", "browser extensions", [
-        LinkItem("https://github.com/7x11x13/hidden-bandcamp-tracks", "hidden-bandcamp-tracks", "detect hidden tracks on bandcamp album pages"),
-        LinkItem("https://github.com/7x11x13/sc-filter", "sc-filter", "filter your soundcloud feed")
-    ]),
-    LinkList("library-projects", "libraries", [
-        LinkItem("https://github.com/7x11x13/soundcloud.py", "soundcloud-v2", "python wrapper for soundcloud's internal api"),
-        LinkItem("https://github.com/7x11x13/youtube-up", "youtube-up", "python library for uploading videos using youtube's internal api"),
-        LinkItem("https://github.com/7x11x13/ez-pyload", "ez-pyload", "python library for downloading files from filesharing sites")
-    ]),
-    LinkList("links", "links", [
-        LinkItem("https://github.com/7x11x13", "github"),
-        LinkItem("https://pypi.org/user/7x11x13/", "pypi")
-    ])
+    LinkList(
+        "web-projects",
+        "web apps",
+        [
+            LinkItem(
+                "https://7x11x13.xyz/crossword/",
+                "crossword",
+                "r/crossword poll archive",
+            ),
+            LinkItem(
+                "https://7x11x13.xyz/last-fm-overlap/",
+                "last-fm-overlap",
+                "view taste overlap of two last.fm users",
+            ),
+            LinkItem(
+                "https://7x11x13.xyz/sc-banner/",
+                "sc-banner",
+                "create seamless soundcloud banners",
+            ),
+            LinkItem(
+                "https://github.com/7x11x13/fx-soundcloud",
+                "fx-soundcloud",
+                "make soundcloud embeds smaller in discord",
+            ),
+        ],
+    ),
+    LinkList(
+        "desktop-projects",
+        "desktop apps",
+        [
+            LinkItem(
+                "https://github.com/7x11x13/songs-to-youtube",
+                "songs-to-youtube",
+                "convert songs to videos and upload them to youtube",
+            ),
+            LinkItem(
+                "https://github.com/7x11x13/free-bandcamp-downloader",
+                "free-bandcamp-downloader",
+                "download $0 minimum music from bandcamp",
+            ),
+            LinkItem(
+                "https://github.com/7x11x13/bandcamp-auto-uploader",
+                "bandcamp-auto-uploader",
+                "upload albums to bandcamp",
+            ),
+        ],
+    ),
+    LinkList(
+        "browser-projects",
+        "browser extensions",
+        [
+            LinkItem(
+                "https://github.com/7x11x13/hidden-bandcamp-tracks",
+                "hidden-bandcamp-tracks",
+                "detect hidden tracks on bandcamp album pages",
+            ),
+            LinkItem(
+                "https://github.com/7x11x13/sc-filter",
+                "sc-filter",
+                "filter your soundcloud feed",
+            ),
+        ],
+    ),
+    LinkList(
+        "library-projects",
+        "libraries",
+        [
+            LinkItem(
+                "https://github.com/7x11x13/selenium-wire-2",
+                "selenium-wire-2",
+                "python library for intercepting requests made by Selenium",
+            ),
+            LinkItem(
+                "https://github.com/7x11x13/soundcloud.py",
+                "soundcloud-v2",
+                "python wrapper for soundcloud's internal api",
+            ),
+            LinkItem(
+                "https://github.com/7x11x13/youtube-up",
+                "youtube-up",
+                "python library for uploading videos using youtube's internal api",
+            ),
+            LinkItem(
+                "https://github.com/7x11x13/ez-pyload",
+                "ez-pyload",
+                "python library for downloading files from filesharing sites",
+            ),
+        ],
+    ),
+    LinkList(
+        "links",
+        "links",
+        [
+            LinkItem("https://github.com/7x11x13", "github"),
+            LinkItem("https://pypi.org/user/7x11x13/", "pypi"),
+            LinkItem("https://www.youtube.com/7x11x13", "youtube"),
+        ],
+    ),
 ]
 
 env = Environment(
